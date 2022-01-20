c7b343183c14ef91a6fb3c22c5965d94b06aad2a
danielrangle <drangelll@outlook.com>
2022-01-20 17:41:59 -0500

    some css changes

---
+++ MyBg/wwwroot/js/reading-speed.js
@@ -0,0 +1,219 @@
+﻿
+const topicBook = {
+    wolf: {
+        text: `The wolf, also known as the gray wolf or grey wolf, is a large canine native to Eurasia
+        and North America. More than thirty subspecies of Canis lupus have been recognized, and gray wolves, as popularly understood,
+        comprise non-domestic/feral subspecies. The wolf is the largest extant member of the family Canidae. <br><br> It is also distinguished
+        from other Canis species by its less pointed ears and muzzle, as well as a shorter torso and a longer tail. The wolf is
+        nonetheless related closely enough to smaller Canis species, such as the coyote and the golden jackal, to produce fertile
+        hybrids with them. <br><br> The banded fur of a wolf is usually mottled white, brown, gray, and black,
+        although subspecies in the arctic region may be nearly all white. Of all members of the genus Canis, the wolf is most specialized
+        for cooperative game hunting as demonstrated by its physical adaptations to tackling large prey, its more social nature, and its highly
+        advanced expressive behaviour. It travels in nuclear families consisting of a mated pair accompanied by their offspring. <br><br>
+        The global wild wolf population was estimated to be 300,000 in
+        2003 and is considered to be of Least Concern by the International Union for Conservation of Nature (IUCN).`,
+        charCount: 208
+    },
+    eagle: {
+        text: `Eagle is the common name for many large birds of prey of the family Accipitridae. Eagles belong to several groups of
+                genera, some of which are closely related. Most of the 60 species of eagle are from Eurasia and Africa.[1] Outside this area, just 14
+                species can be found—2 in North America, 9 in Central and South America, and 3 in Australia.<br> <br>
+                Eagles are not a natural group but denote essentially any bird of prey large enough to hunt sizeable (about 50 cm long or more overall)
+                vertebrate prey. <br><br>
+                Eagles are large, powerfully built birds of prey, with heavy heads and beaks. Even the smallest eagles, such as the booted eagle
+                (Aquila pennata), which is comparable in size to a common buzzard (Buteo buteo) or red-tailed hawk (B. jamaicensis), have relatively longer
+                and more evenly broad wings, and more direct, faster flight – despite the reduced size of aerodynamic feathers. Most eagles are larger than
+                any other raptors apart from some vultures. The smallest species of eagle is the South Nicobar serpent eagle (Spilornis klossi), at 450 g (1 lb)
+                and 40 cm (16 in). The largest species are discussed below. Like all birds of prey, eagles have very large hooked beaks for ripping flesh from their prey, strong, muscular legs, and powerful talons. `,
+        charCount: 214
+    },
+    rhino: {
+        text: `A rhinoceros commonly abbreviated to rhino, is a member of any of the five extant species (or numerous extinct species) of odd-toed
+            ungulates in the family Rhinocerotidae. (It can also refer to a member of any of the extinct species of the superfamily Rhinocerotoidea.) Two of the extant species are native to Africa, and three to South and Southeast Asia.<br><br>
+            Rhinoceroses are some of the largest remaining megafauna: all weigh at least one tonne in adulthood. They have a herbivorous diet, small brains (400–600 g) for mammals of their size, one or two horns, and a thick (1.5–5 cm),
+            protective skin formed from layers of collagen positioned in a lattice structure. They generally eat leafy material, although their ability to ferment food
+            in their hindgut allows them to subsist on more fibrous plant matter when necessary. Unlike other perissodactyls, the two African species of rhinoceros lack
+            teeth at the front of their mouths; they rely instead on their lips to pluck food.<br><br>
+            Rhinoceros are killed by poachers for their horns, which are bought and sold on the black market for high prices, leading to most living rhinoceros species
+            being considered endangered.`,
+        charCount: 191
+    },
+    bison: {
+        text: `Bison are large, even-toed ungulates in the genus Bison within the subfamily Bovinae. Two extant and six extinct species are recognised.
+                Of the six extinct species, five became extinct in the Quaternary extinction event. Bison palaeosinensis evolved in the Early Pleistocene in South Asia, and
+                was the evolutionary ancestor of B. priscus (steppe bison), which was the ancestor of all other Bison species. From 2 million years ago to 6,000 BC, steppe
+                bison ranged across the mammoth steppe, inhabiting Europe and northern Asia with B. schoetensacki (woodland bison), and North America with B. antiquus, B.
+                latifrons, and B. occidentalis. The last species to go extinct, B. occidentalis, was succeeded at 3,000 BC by B. bison.<br><br>
+                Of the two surviving species, the American bison, B. bison, found only in North America, is the more numerous. Although colloquially referred to as a
+                buffalo in the United States and Canada,[2] it is only distantly related to the true buffalo. The North American species is composed of two subspecies,
+                the Plains bison, B. b. bison, and the wood bison, B. b. athabascae, which is the namesake of Wood Buffalo National Park in Canada. `,
+        charCount: 189
+    },
+    mammoth: {
+        text: `A mammoth is any species of the extinct elephantid genus Mammuthus, one of the many genera that make up the order of trunked mammals
+                called proboscideans. The various species of mammoth were commonly equipped with long, curved tusks and, in northern species, a covering of long hair.<br><br>
+                They lived from the Pliocene epoch (from around 5 million years ago) into the Holocene at about 4,000 years ago, and various species existed in Africa,
+                Europe, Asia, and North America. They were members of the family Elephantidae, which also contains the two genera of modern elephants and their ancestors.
+                Mammoths are more closely related to living Asian elephants than African Elephants.<br><br>
+                The oldest representative of Mammuthus, the South African mammoth (M. subplanifrons), appeared around 5 million years ago during the early Pliocene
+                in what is now southern and eastern Africa. Descendant species of these mammoths moved north and continued to propagate into numerous subsequent species,
+                eventually covering most of Eurasia before extending into the Americas at least 600,000 years ago. The last species to emerge, the woolly mammoth
+                (M. primigenius), developed about 400,000 years ago in East Asia, with some surviving on Russia's Wrangel Island in the Arctic Ocean until as
+                recently as roughly 3,700 to 4,000 years ago, still extant during the construction of the Great Pyramid of ancient Egypt.`,
+        charCount: 219
+    },
+    mamba: {
+        text: `The eastern green mamba (Dendroaspis angusticeps) is a highly venomous snake species of the mamba genus Dendroaspis native to the coastal
+                regions of southern East Africa. Described by Scottish surgeon and zoologist Andrew Smith in 1849, it has a slender build with a bright green back and green-yellow
+                ventral scales. Adult females average around 2 metres (6 ft 7 in) in length, and males are slightly smaller.<br><br>
+                A shy and elusive species, the eastern green mamba is rarely seen. This elusiveness is usually attributed to its arboreal habitat and green colouration,
+                which acts as camouflage in its natural environment. It has also been observed to use ambush predation, like many vipers, contrary to the active foraging style
+                typical of other elapid snakes. It preys on birds, eggs, bats, and rodents such as mice, rats, and gerbils.<br><br>
+                Its venom consists of both neurotoxins and cardiotoxins. Symptoms of envenomation include swelling of the injection site, dizziness, and nausea, accompanied
+                by difficulty breathing and swallowing, irregular heartbeat, and convulsions progressing to respiratory paralysis. Bites that result in severe envenomation can quickly be fatal.`,
+        charCount: 180
+    },
+    shark: {
+        text: `Sharks are a group of elasmobranch fish characterized by a cartilaginous skeleton, five to seven gill slits on the sides of the head,
+                and pectoral fins that are not fused to the head. Modern sharks are classified within the clade Selachimorpha (or Selachii) and are the sister group
+                to the rays. However, the term "shark" has also been used to refer to extinct members of the subclass Elasmobranchii, which are technically outside the
+                Selachimorpha clade. Notable examples of improper classification include Cladoselache, Xenacanthus, and various other members of the Chondrichthyes class like the holocephalid eugenedontidans.<br><br>
+                Under this broader definition, the earliest known sharks date back to more than 420 million years ago. Acanthodians are often referred to as "spiny sharks"; though they
+                are not part of Chondrichthyes proper, they are a paraphyletic assemblage leading to cartilaginous fish as a whole. <br><br>Since then, sharks have diversified into over 500 species.
+                They range in size from the small dwarf lanternshark (Etmopterus perryi), a deep sea species that is only 17 centimetres (6.7 in) in length, to the whale shark (Rhincodon typus),
+                the largest fish in the world, which reaches approximately 12 metres (40 ft) in length. Sharks are found in all seas and are common to depths up to 2,000 metres (6,600 ft).`,
+        charCount: 211
+    }
+}
+
+const readerTextBox = document.querySelector(".reader-text-box");
+const clockContainer = document.querySelector(".container");
+const totalTimeElapsedMsg = document.querySelector(".reading-time-result");
+const yourResultsWrapper = document.querySelector(".reading-result-wrapper")
+
+// buttons 
+const wolfBtn = document.querySelector("#button-wolf");
+const eagleBtn = document.querySelector("#button-eagle");
+const rhinoBtn = document.querySelector("#button-rhino");
+const bisonBtn = document.querySelector("#button-bison");
+const mammothBtn = document.querySelector("#button-mammoth");
+const mambaBtn = document.querySelector("#button-mamba");
+const sharkBtn = document.querySelector("#button-shark");
+
+let startTime;
+let wordCount;
+
+const choseTopic = (topic) => {
+    readerTextBox.style.display = "none";
+    clockContainer.style.display = "none";
+
+    if (topic === 'wolf') {
+        readerTextBox.innerHTML = topicBook.wolf.text;
+        wordCount = topicBook.wolf.charCount;
+
+        eagleBtn.classList.remove('active');
+        rhinoBtn.classList.remove('active');
+        bisonBtn.classList.remove('active');
+        mammothBtn.classList.remove('active');
+        mambaBtn.classList.remove('active');
+        sharkBtn.classList.remove('active');
+
+        wolfBtn.classList.add('active');
+    } else if (topic === 'eagle') {
+        readerTextBox.innerHTML = topicBook.eagle.text;
+        wordCount = topicBook.eagle.charCount;
+
+        rhinoBtn.classList.remove('active');
+        bisonBtn.classList.remove('active');
+        wolfBtn.classList.remove('active');
+        mammothBtn.classList.remove('active');
+        mambaBtn.classList.remove('active');
+        sharkBtn.classList.remove('active');
+
+        eagleBtn.classList.add('active');
+    }
+    else if (topic === 'rhino') {
+        readerTextBox.innerHTML = topicBook.rhino.text;
+        wordCount = topicBook.rhino.charCount;
+
+        eagleBtn.classList.remove('active');
+        bisonBtn.classList.remove('active');
+        wolfBtn.classList.remove('active');
+        mammothBtn.classList.remove('active');
+        mambaBtn.classList.remove('active');
+        sharkBtn.classList.remove('active');
+
+        rhinoBtn.classList.add('active');
+    }
+    else if (topic === 'bison') {
+        readerTextBox.innerHTML = topicBook.bison.text;
+        wordCount = topicBook.bison.charCount;
+
+        eagleBtn.classList.remove('active');
+        rhinoBtn.classList.remove('active');
+        wolfBtn.classList.remove('active');
+        mammothBtn.classList.remove('active');
+        mambaBtn.classList.remove('active');
+        sharkBtn.classList.remove('active');
+
+        bisonBtn.classList.add('active');
+    }
+    else if (topic === 'mammoth') {
+        readerTextBox.innerHTML = topicBook.mammoth.text;
+        wordCount = topicBook.mammoth.charCount;
+
+        eagleBtn.classList.remove('active');
+        rhinoBtn.classList.remove('active');
+        wolfBtn.classList.remove('active');
+        bisonBtn.classList.remove('active');
+        mambaBtn.classList.remove('active');
+        sharkBtn.classList.remove('active');
+
+        mammothBtn.classList.add('active');
+    }
+    else if (topic === 'mamba') {
+        readerTextBox.innerHTML = topicBook.mamba.text;
+        wordCount = topicBook.mamba.charCount;
+
+        eagleBtn.classList.remove('active');
+        rhinoBtn.classList.remove('active');
+        wolfBtn.classList.remove('active');
+        mammothBtn.classList.remove('active');
+        sharkBtn.classList.remove('active');
+        bisonBtn.classList.remove('active');
+
+        mambaBtn.classList.add('active');
+    }
+    else if (topic === 'shark') {
+        readerTextBox.innerHTML = topicBook.shark.text;
+        wordCount = topicBook.shark.charCount;
+
+        eagleBtn.classList.remove('active');
+        rhinoBtn.classList.remove('active');
+        wolfBtn.classList.remove('active');
+        mammothBtn.classList.remove('active');
+        mambaBtn.classList.remove('active');
+        bisonBtn.classList.remove('active');
+
+        sharkBtn.classList.add('active');
+    }
+}
+
+const startReading = () => {
+    yourResultsWrapper.style.display = "none"
+    startTime = Date.now()
+    readerTextBox.style.display = "block";
+    clockContainer.style.display = "flex";
+}
+
+const stopReading = () => {
+
+    const elapsedtime = ((Date.now() - startTime) / 1000);
+    const wordsPerMinute = Math.ceil((wordCount / elapsedtime) * 60)
+
+
+    clockContainer.style.display = "none";
+    yourResultsWrapper.style.display = "block"
+    totalTimeElapsedMsg.innerHTML = `Your reading speed is ${wordsPerMinute} words per minute! 🎉 <br> The average reading speed in America is 220-250 words per minute. ⏰`
+
+}
